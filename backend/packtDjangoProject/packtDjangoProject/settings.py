6d0de7157e13c32363ba6bf066101f158de7dabe
Ramon Lopez <ramonmartinl69@gmail.com>
2024-05-22 15:24:51 +0200

    Use default sqlite3 DB

--- backend/packtDjangoProject/packtDjangoProject/settings.py
+++ backend/packtDjangoProject/packtDjangoProject/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'packtDjangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'packtdjango-db',
-        'USER': 'sa',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'packtdjango-db',
+#         'USER': 'sa',
+#         'PASSWORD': 'admin',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
