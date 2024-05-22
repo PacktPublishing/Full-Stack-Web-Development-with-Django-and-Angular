417d5b301572110be8e03201aa06d9ce01ddd803
Ramon Lopez <ramonmartinl69@gmail.com>
2024-05-22 15:17:39 +0200

    fix bug with env variables

--- backend/packtDjangoProject/packtDjangoProject/settings.py
+++ backend/packtDjangoProject/packtDjangoProject/settings.py
@@ -73,28 +73,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'packtDjangoProject.wsgi.application'
 
 # Your secret key
-SECRET_KEY = getenv("SECRET_KEY")
+#SECRET_KEY = getenv("SECRET_KEY")
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'packtdjango-db',
-#         'USER': 'sa',
-#         'PASSWORD': 'admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'packtdjango-db',
+        'USER': 'sa',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
