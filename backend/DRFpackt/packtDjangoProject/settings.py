fef23bf3ac83a2459df113d65a7555687840d6c6
Ramon Lopez <ramonmartinl69@gmail.com>
2024-06-26 15:22:16 +0200

    Customize SimpleJWT

--- backend/DRFpackt/packtDjangoProject/settings.py
+++ backend/DRFpackt/packtDjangoProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import environ
 
@@ -156,4 +157,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
