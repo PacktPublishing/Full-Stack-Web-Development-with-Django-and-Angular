6f3bfd8ae17bb2da33d94dbe87ac6da991b7c1de
Ramon Lopez <ramonmartinl69@gmail.com>
2024-07-10 16:10:36 +0200

    generate first component (product) with Angular CLI

--- frontend/angularpackt-ui/src/app/app.component.spec.ts
+++ frontend/angularpackt-ui/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angularpackt-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularpackt-UI');
+    expect(app.title).toEqual('angularpackt-ui');
   });
 
   it('should render title', () => {
---
+++ frontend/angularpackt-ui/src/app/components/product/product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apui-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+}
