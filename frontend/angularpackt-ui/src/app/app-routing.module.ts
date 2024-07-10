57bcbb8d882ed669f91f16138c38403b1fad694b
Ramon Lopez <ramonmartinl69@gmail.com>
2024-07-10 17:18:56 +0200

    add route for product component

--- frontend/angularpackt-ui/src/app/app-routing.module.ts
+++ frontend/angularpackt-ui/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product', component: ProductComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
